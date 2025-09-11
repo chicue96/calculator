737907cd2e283231cf87ff8bf14f37e2ca8c84f6
colibri <chicue96@gmail.com>
2025-09-10 19:29:26 -0500

    Pop OS

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Terminal Calculator")
+print("Type 'exit' to quit")
+print("Type the number of the operation you want to perform:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
