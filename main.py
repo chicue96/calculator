d3f0e8b466b3f89edd95b0f2c11720652edcf32a
colibri <chicue96@gmail.com>
2025-09-10 19:50:55 -0500

    Pop OS

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-print("Terminal Calculator")
-print("Type 'exit' to quit")
-print("Type the number of the operation you want to perform:")
-print("1. Addition (+)")
-print("2. Subtraction (-)")
-print("3. Multiplication (*)")
-print("4. Division (/)")
+def main():
+    print("Terminal Calculator")
+    print("Type 'exit' to quit")
+    print("Type the number of the operation you want to perform:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+
+if __name__ == "__main__": # solo corre si ejecuto main.py directamente
+    main()
